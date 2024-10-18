9bd0133c4fbdd1184d3bd00ef7641ca7c8266c8a
Shamazam <shamalfiji@gmail.com>
2024-10-18 16:53:59 +1200

    changes

---
+++ src/test/java/com/example/Asg2CS241/Repository/RepositoryTest.java
@@ -0,0 +1,11 @@
+package com.example.Asg2CS241.Repository;
+
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+public class RepositoryTest {
+
+}
